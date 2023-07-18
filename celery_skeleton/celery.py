b342e9b6e668fdcf7a1e7903e4e3007aef8bad1d
K M Emon Ahmed <emon@ticonsys.com>
2023-07-18 13:11:01 +0600

    comment periodic task from celery.py

--- celery_skeleton/celery.py
+++ celery_skeleton/celery.py
@@ -17,10 +17,10 @@ app.config_from_object(settings, namespace='CELERY')
 # celery beat settings
 app.conf.beat_schedule = {
     # example task to be executed every 30 seconds
-    'add-every-30-seconds': {
-        'task': 'examples.celery_beat_test.tasks.test_celery_beat',
-        'schedule': crontab(hour=12, minute=51),
-    },
+    # 'add-every-30-seconds': {
+    #     'task': 'examples.celery_beat_test.tasks.test_celery_beat',
+    #     'schedule': 30.0,
+    # },
 }
 
 
